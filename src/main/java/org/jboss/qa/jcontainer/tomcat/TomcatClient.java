0a2a7e9b9716b86c88e23eb2771811a516ef511c
Martin Basovnik <mbasovni@redhat.com>
2015-04-16 10:23:08 +0200

    Issue #30 Add support for Tomcat container
    - Closes #30

---
+++ src/main/java/org/jboss/qa/jcontainer/tomcat/TomcatClient.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 Red Hat Inc. and/or its affiliates and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.qa.jcontainer.tomcat;
+
+import org.jboss.qa.jcontainer.Client;
+
+import java.io.IOException;
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TomcatClient<T extends TomcatConfiguration> extends Client<T> {
+
+	private static final String ERROR_MSG = "Tomcat container does not support client";
+
+	public TomcatClient(T configuration) {
+		super(configuration);
+	}
+
+	@Override
+	public boolean isConnected() {
+		return false;
+	}
+
+	@Override
+	protected void connectInternal() throws Exception {
+		throw new UnsupportedOperationException(ERROR_MSG);
+	}
+
+	@Override
+	protected void executeInternal(String command) throws Exception {
+		throw new UnsupportedOperationException(ERROR_MSG);
+	}
+
+	@Override
+	protected void executeInternal(List<String> commands) throws Exception {
+		throw new UnsupportedOperationException(ERROR_MSG);
+	}
+
+	@Override
+	protected void closeInternal() throws IOException {
+		throw new UnsupportedOperationException(ERROR_MSG);
+	}
+}
