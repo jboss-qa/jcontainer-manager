9cc3e4ef1d7937f3444c8554d5b076398761b0fe
tveskrna <tveskrna@redhat.com>
2023-02-02 16:48:25 +0100

    [eap-jdk17] Remove unsupported option for jdk17

--- containers/eap/src/main/java/org/jboss/qa/jcontainer/eap/EapConfiguration.java
+++ containers/eap/src/main/java/org/jboss/qa/jcontainer/eap/EapConfiguration.java
@@ -39,7 +39,10 @@ public class EapConfiguration extends WildflyConfiguration {
 			super();
 			xms("1303m");
 			xmx("1303m");
-			maxPermSize("256m");
+			//-XX:MaxPermSize has been removed from jdk17
+			if (Integer.parseInt(System.getProperty("java.version").split("\\.")[0]) < 17) {
+				maxPermSize("256m");
+			}
 		}
 
 		public EapConfiguration build() {
